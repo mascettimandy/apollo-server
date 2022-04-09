8876463428f4ea6df9e6b2d10ad9ecfd0ad9583a
mascettimandy <103158271+mascettimandy@users.noreply.github.com>
2022-04-09 18:38:48 -0400

    time complete

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type LINK {
+const typeDefs = `
+  type Link {
     url: String
     slug: String
   }
 `;
 
+const cache = LRU({ max: 50, maxAge: 1000 * 60 * 60 });
+
 // Provide resolver functions for your schema fields
 const resolvers = {
-  Query: {
-    url: (root, args, context) => "Mandy",
-    slug: (root, args, context) => "HEY"
+  Query: { 
+    getUrl: () => {
+       return{
+     url: (root, args, context) => "Mandy",
+     slug: (root, args, context) => "HEY"  
   }
-};
+ Mutation: {
+  createUrl(input: Link): String!
+ }
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs,z
   resolvers
 });
 
